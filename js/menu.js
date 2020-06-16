516145f3ad65dcfb5fc8d03c04afac79d8207a5b
DunZek <60448983+DunZek@users.noreply.github.com>
2020-06-16 15:37:43 -0600

    Second commit
    User functionality improved

--- js/menu.js
+++ js/menu.js
@@ -6,15 +6,38 @@ const header = document.querySelector('header')
 const hr = document.querySelector('hr')
 const content = document.querySelector('#content')
 
-open_menu.addEventListener('click', () => {
-    misc.classList.add('open')
-    header.classList.add('blur')
-    hr.classList.add('blur')
-    content.classList.add('blur')
-})
-close_menu.addEventListener('click', () => {
-    misc.classList.remove('open')
-    header.classList.remove('blur')
-    hr.classList.remove('blur')
-    content.classList.remove('blur')
-})
+let open = false
+
+open_menu.addEventListener('click', _menu)
+
+close_menu.addEventListener('click', _menu)
+
+header.addEventListener('click', _menu)
+
+hr.addEventListener('click', _menu)
+
+content.addEventListener('click', _menu)
+
+function _menu() {
+    if (open == false){
+        setTimeout(() => {
+            misc.classList.add('open')
+            header.classList.add('blur')
+            hr.classList.add('blur')
+            content.classList.add('blur')
+
+            console.log(open)
+            return open = true
+        },10)
+    } else if (open == true) {
+        setTimeout(() => {
+            misc.classList.remove('open')
+            header.classList.remove('blur')
+            hr.classList.remove('blur')
+            content.classList.remove('blur')
+
+            console.log(open)
+            return open = false
+        },10)
+    }
+}
